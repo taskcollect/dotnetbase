1eda25e575db3e686cd242f04d88b4263b116368
xxori <patrick@patrick-arch.localdomain>
2021-09-19 11:58:41 +0930

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+/* HEY */
+
+using System;
+
+namespace dotnetapp {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("hello, world!");
+        }
+    }
+}
